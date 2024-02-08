e001002c2dd8955d0a5bf59ff95b3e90edd4f97a
T8 <108910965+T888R@users.noreply.github.com>
2024-02-07 21:04:32 -0700

    adding deletion of specific items and remove pop off stack

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
@@ -10,7 +10,27 @@ function App() {
   );
 }
 
-//[1, 2, 3].map((item) => `the number is ${item}`) // returns ["the number is 1", "the number is 2", "the number is 3"]
+function TodoBubble({ index, item, removeItem }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
+
+  return (
+    <div className={isChecked === true ? "finished" : "bubble"}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <h5>{item}</h5>
+      <button onClick={() => removeItem(index)}>Delete</button>
+    </div>
+  );
+}
+
+// [1, 2, 3].map((item) => `the number is ${item}`) // returns ["the number is 1", "the number is 2", "the number is 3"]
 
 // [1, 2, 3].map((item) => <div>{item}</div>); // returns [<div>1</div>, <div>2</div>, <div>3</div>]
 
@@ -28,29 +48,40 @@ function RenderMultipleThings() {
   };
 
   const loadItemsToItems = async () => {
-    const response = await axios.get("http://localhost:3000", {
-      params: {
-        value: "",
-      },
-    });
-
-    setValue(response.data);
-    console.log(response.value);
+    const response = await axios.get("http://localhost:3000");
+    setItems(response.data);
   };
+
+  const removeItem = async (index) => {
+    await axios.delete("http://localhost:3000", { data: { index } });
+    loadItemsToItems();
+  };
+
+  useEffect(() => {
+    loadItemsToItems();
+  }, []);
 
   return (
     <>
-      <div>
+      <div className="todotext">
         <label>
           Thing to do:
           <input type="text" value={value} onChange={handleChange} />
         </label>
-        <button onClick={addNewItemToItems}>Create New Thing</button>
-        <button onClick={loadItemsToItems}>Load Things</button>
       </div>
-      {items.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
+      <div className="buttons">
+        <button onClick={addNewItemToItems}>Create New Thing</button>
+      </div>
+      <div className="bubbleGroup">
+        {items.map((item, index) => (
+          <TodoBubble
+            removeItem={removeItem}
+            key={index}
+            item={item}
+            index={index}
+          ></TodoBubble>
+        ))}
+      </div>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,32 +1,47 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("node:fs/promises");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
-const options = {
-  headers: { "content-type": "application/json" },
-};
 
 app.use(express.json());
 app.use(cors());
 
+const rawData = fs.readFileSync("./text-data.json", "utf-8");
+const data = JSON.parse(rawData); // data === ["hi"]
+
 app.get("/", async (req, res) => {
   try {
-    const rawData = await fs.readFile("./text-data.json", "utf-8");
-    const data = JSON.parse(rawData);
-    console.log(data);
     res.json(data);
+    console.log(data);
   } catch (err) {
     console.error(err);
+    res.status(400).send();
   }
 });
 
 app.post("/", async (req, res) => {
   try {
-    const writableText = await fs.writeFile(
+    data.push(req.body.value);
+    console.log(req.body.index);
+    const writableText = fs.writeFileSync(
       "./text-data.json",
-      JSON.stringify(req.body)
+      JSON.stringify(data),
+    );
+  } catch (err) {
+    console.error(err);
+  }
+  res.send();
+});
+
+app.delete("/", async (req, res) => {
+  try {
+    console.log(req.body.index);
+    data.splice(req.body.index, 1);
+    const deleteText = fs.writeFileSync(
+      "./text-data.json",
+      JSON.stringify(data),
     );
   } catch (err) {
     console.error(err);
